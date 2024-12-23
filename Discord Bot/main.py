2cfb8c41f10f9268be7eaef778e8295c34d1e99f
Sammy_W <sw@sammywong.dev>
2024-12-22 19:05:21 -0800

    Alligning main.py variables

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -2,11 +2,11 @@ import discord
 from utils import file
 
 command = "something"
-token = file.getDiscordToken()
+token = file.getDiscordKey()
 
 client = discord.Client()
 @client.event
 async def on_message(message):
     if message == command:
-        ...
+        print("Hello, World")
 client.run(token)
